e7d13c5897712201ad3646b699ea184e75c8f48f
Irina Fedchenko <irina.fedchenko.00@gmail.com>
2023-05-13 23:34:15 +0600

    commit one

---
+++ src/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./header.css"
+import imageOne from "../store/image/image-3.jpg"
+const Header = () => {
+    return (
+        <div>
+            qwere
+            <h1>header header</h1>
+            <img src={imageOne} alt="qwe"/>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/sidebar/sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./sidebar.css"
+
+const Sidebar = () => {
+    return (
+        <div>
+            <h2>Sidebar</h2>
+        </div>
+    );
+};
+
+export default Sidebar;
